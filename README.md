8cecbe5d5f02c904bee12180ae61f87796a3ba14
arturovt <arthurandrosovich@gmail.com>
2022-07-03 22:52:57 +0300

    feat: upgrade to Angular 14

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# autopublish-template
+# angular-material
 
-[![npm version](https://img.shields.io/npm/v/@esm-bundle/autopublish-template.svg?style=flat)](https://www.npmjs.com/package/@esm-bundle/autopublish-template) [![build status](https://travis-ci.com/esm-bundle/autopublish-template.svg?branch=master)](https://travis-ci.com/esm-bundle/autopublish-template) [![](https://data.jsdelivr.com/v1/package/npm/@esm-bundle/autopublish-template/badge)](https://www.jsdelivr.com/package/npm/@esm-bundle/autopublish-template)
+[![npm version](https://img.shields.io/npm/v/@esm-bundle/angular__material.svg?style=flat)](https://www.npmjs.com/package/@esm-bundle/angular__material) [![build status](https://travis-ci.com/esm-bundle/angular__material.svg?branch=master)](https://travis-ci.com/esm-bundle/angular__material) [![](https://data.jsdelivr.com/v1/package/npm/@esm-bundle/angular__material/badge)](https://www.jsdelivr.com/package/npm/@esm-bundle/angular__material)
 
 ["What is this" blog post](https://medium.com/@joeldenning/an-esm-bundle-for-any-npm-package-5f850db0e04d)
 
@@ -8,28 +8,46 @@
 
 A repo that tests automatic detection of upstream publishes and the publishing of `@esm-bundle` versions of them.
 
-Upstream repo at https://github.com/joeldenning/autopublish-template.
+## Latest Version
 
-## JS Delivr
+### @angular/material/autocomplete
 
-https://cdn.jsdelivr.net/npm/@esm-bundle/autopublish-template/esm/index.js
+#### ES2015
 
-https://cdn.jsdelivr.net/npm/@esm-bundle/autopublish-template/system/index.js
+https://cdn.jsdelivr.net/npm/@esm-bundle/angular__material/system/es2015/ivy/angular-autocomplete.js
 
-## Unpkg
+https://cdn.jsdelivr.net/npm/@esm-bundle/angular__material/system/es2015/ivy/angular-autocomplete.min.js
 
-https://unpkg.com/@esm-bundle/autopublish-template/esm/index.js
+https://unpkg.com/@esm-bundle/angular__material/system/es2015/ivy/angular-autocomplete.js
 
-https://unpkg.com/@esm-bundle/autopublish-template/system/index.js
+https://unpkg.com/@esm-bundle/angular__material/system/es2015/ivy/angular-autocomplete.min.js
+
+#### ES2020
+
+https://cdn.jsdelivr.net/npm/@esm-bundle/angular__material/system/es2020/ivy/angular-autocomplete.js
+
+https://cdn.jsdelivr.net/npm/@esm-bundle/angular__material/system/es2020/ivy/angular-autocomplete.min.js
+
+https://unpkg.com/@esm-bundle/angular__material/system/es2020/ivy/angular-autocomplete.js
+
+https://unpkg.com/@esm-bundle/angular__material/system/es2020/ivy/angular-autocomplete.min.js
+
+## Other packages
+
+Other packages are named in the following format `angular-${material-package-filename}`, for instance, `@angular/material/tooltip` is `angular-tooltip.js`, `@angular/material/table` is `angular-table.js`, etc.
+
+## Need a build that doesn't exist yet?
+
+If you need an es5 build, non-ivy build, or a build for a different version of Angular, please open a Github issue.
 
 ## Npm
 
 ```sh
-npm install --save autopublish-template@npm:@esm-bundle/autopublish-template
+npm install --save angular__material@npm:@esm-bundle/angular__material
 ```
 
 ## Yarn
 
 ```sh
-yarn add autopublish-template@npm:@esm-bundle/autopublish-template
+yarn add angular__material@npm:@esm-bundle/angular__material
 ```
--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,204 @@
-describe("@esm-bundle/autopublish-template", () => {
-  it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
-  });
+describe("@esm-bundle/angular__material", () => {
+  const packages = [
+    {
+      packageName: "@angular/material/autocomplete",
+      filename: "angular-autocomplete",
+      expectedImport: "MatAutocomplete",
+    },
+    {
+      packageName: "@angular/material/badge",
+      filename: "angular-badge",
+      expectedImport: "MatBadge",
+    },
+    {
+      packageName: "@angular/material/bottom-sheet",
+      filename: "angular-bottom-sheet",
+      expectedImport: "MatBottomSheet",
+    },
+    {
+      packageName: "@angular/material/button-toggle",
+      filename: "angular-button-toggle",
+      expectedImport: "MatButtonToggle",
+    },
+    {
+      packageName: "@angular/material/button",
+      filename: "angular-button",
+      expectedImport: "MatButton",
+    },
+    {
+      packageName: "@angular/material/card",
+      filename: "angular-card",
+      expectedImport: "MatCard",
+    },
+    {
+      packageName: "@angular/material/checkbox",
+      filename: "angular-checkbox",
+      expectedImport: "MatCheckbox",
+    },
+    {
+      packageName: "@angular/material/chips",
+      filename: "angular-chips",
+      expectedImport: "MatChip",
+    },
+    {
+      packageName: "@angular/material/core",
+      filename: "angular-core",
+      expectedImport: "DateAdapter",
+    },
+    {
+      packageName: "@angular/material/datepicker",
+      filename: "angular-datepicker",
+      expectedImport: "MatDatepicker",
+    },
+    {
+      packageName: "@angular/material/dialog",
+      filename: "angular-dialog",
+      expectedImport: "MatDialog",
+    },
+    {
+      packageName: "@angular/material/divider",
+      filename: "angular-divider",
+      expectedImport: "MatDivider",
+    },
+    {
+      packageName: "@angular/material/expansion",
+      filename: "angular-expansion",
+      expectedImport: "MatExpansionPanel",
+    },
+    {
+      packageName: "@angular/material/form-field",
+      filename: "angular-form-field",
+      expectedImport: "MatFormField",
+    },
+    {
+      packageName: "@angular/material/grid-list",
+      filename: "angular-grid-list",
+      expectedImport: "MatGridList",
+    },
+    {
+      packageName: "@angular/material/icon",
+      filename: "angular-icon",
+      expectedImport: "MatIcon",
+    },
+    {
+      packageName: "@angular/material/input",
+      filename: "angular-input",
+      expectedImport: "MatInput",
+    },
+    {
+      packageName: "@angular/material/list",
+      filename: "angular-list",
+      expectedImport: "MatList",
+    },
+    {
+      packageName: "@angular/material/menu",
+      filename: "angular-menu",
+      expectedImport: "MatMenu",
+    },
+    {
+      packageName: "@angular/material/paginator",
+      filename: "angular-paginator",
+      expectedImport: "MatPaginator",
+    },
+    {
+      packageName: "@angular/material/progress-bar",
+      filename: "angular-progress-bar",
+      expectedImport: "MatProgressBar",
+    },
+    {
+      packageName: "@angular/material/progress-spinner",
+      filename: "angular-progress-spinner",
+      expectedImport: "MatProgressSpinner",
+    },
+    {
+      packageName: "@angular/material/progress-bar",
+      filename: "angular-progress-bar",
+      expectedImport: "MatProgressBar",
+    },
+    {
+      packageName: "@angular/material/radio",
+      filename: "angular-radio",
+      expectedImport: "MatRadioButton",
+    },
+    {
+      packageName: "@angular/material/select",
+      filename: "angular-select",
+      expectedImport: "MatSelect",
+    },
+    {
+      packageName: "@angular/material/sidenav",
+      filename: "angular-sidenav",
+      expectedImport: "MatSidenav",
+    },
+    {
+      packageName: "@angular/material/slide-toggle",
+      filename: "angular-slide-toggle",
+      expectedImport: "MatSlideToggle",
+    },
+    {
+      packageName: "@angular/material/slider",
+      filename: "angular-slider",
+      expectedImport: "MatSlider",
+    },
+    {
+      packageName: "@angular/material/snack-bar",
+      filename: "angular-snack-bar",
+      expectedImport: "MatSnackBar",
+    },
+    {
+      packageName: "@angular/material/sort",
+      filename: "angular-sort",
+      expectedImport: "MatSort",
+    },
+    {
+      packageName: "@angular/material/stepper",
+      filename: "angular-stepper",
+      expectedImport: "MatStepper",
+    },
+    {
+      packageName: "@angular/material/table",
+      filename: "angular-table",
+      expectedImport: "MatTable",
+    },
+    {
+      packageName: "@angular/material/tabs",
+      filename: "angular-tabs",
+      expectedImport: "MatTab",
+    },
+    {
+      packageName: "@angular/material/toolbar",
+      filename: "angular-toolbar",
+      expectedImport: "MatToolbar",
+    },
+    {
+      packageName: "@angular/material/tooltip",
+      filename: "angular-tooltip",
+      expectedImport: "MatTooltip",
+    },
+    {
+      packageName: "@angular/material/tree",
+      filename: "angular-tree",
+      expectedImport: "MatTree",
+    },
+  ];
 
-  it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+  packages.forEach(({ packageName, filename, expectedImport }) => {
+    describe(packageName, () => {
+      ["es2015", "es2020"].forEach((ecma) => {
+        it(`can load the System.register ${ecma} bundle`, async () => {
+          const m = await System.import(
+            `/base/system/${ecma}/ivy/${filename}.js`
+          );
+          expect(m[expectedImport]).toBeDefined();
+        });
+
+        it(`can load the System.register ${ecma} prod bundle`, async () => {
+          const m = await System.import(
+            `/base/system/${ecma}/ivy/${filename}.min.js`
+          );
+          expect(m[expectedImport]).toBeDefined();
+        });
+      });
+    });
   });
 });
